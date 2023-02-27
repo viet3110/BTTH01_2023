4167956ca507d907bb7d2370c8394745238102bd
viet3110 <104310094+viet3110@users.noreply.github.com>
2023-02-27 07:14:46 +0700

    Add files via upload

--- chuc_nang/thembaiviet.php
+++ chuc_nang/thembaiviet.php
@@ -8,11 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style_login.css">
-    
-    <title>Thêm tác giả </title>
 </head>
 <body>
-<header>
+    <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
             <div class="container-fluid">
                 <div class="h3">
@@ -24,7 +22,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a href="trangchu.php" class="nav-link" aria-current="page" href="./">Trang chủ</a>
+                        <a href="trangchu.php"class="nav-link" aria-current="page" href="./">Trang chủ</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="../index.php">Trang ngoài</a>
@@ -33,10 +31,10 @@
                         <a href =" themtheloai.php" class="nav-link active " href="category.php">Thể Loại</a>
                     </li>
                     <li class="nav-item">
-                        <a  href =" themtacgia.php" class="nav-link active fw-bold" href="author.php">Tác giả</a>
+                        <a href =" themtacgia.php" class="nav-link" href="author.php">Tác giả</a>
                     </li>
                     <li class="nav-item">
-                        <a href =" thembaiviet.php" class="nav-link " href="article.php">Bài viết</a>
+                        <a href =" thembaiviet.php" class="nav-link active fw-bold" href="article.php">Bài viết</a>
                     </li>
                 </ul>
                 </div>
@@ -44,13 +42,19 @@
         </nav>
 
     </header>
+    
     <!-- <?php require_once 'connect.php' ?> -->
 
     <?php
     if(isset($_POST['add'])){
-        $ten_tgia =$_POST['ten_tgia'];
+        $ma_bviet =$_POST['ma_bviet'];
+        $tieude =$_POST['tieude'];
+        $ten_bhat =$_POST['ten_bhat'];
+        $ma_tloai =$_POST['ma_tloai'];
+        $tomtat =$_POST['tomtat'];
         $ma_tgia =$_POST['ma_tgia'];
-        if($conn -> query("INSERT INTO tacgia (ten_tgia,ma_tgia) VALUES ('$ten_tgia','$ma_tgia') ")){
+        // $f =$_POST['ngayviet'];
+        if($conn -> query("INSERT INTO baiviet (ma_bviet, tieude,ten_bhat,ma_tloai,tomtat,ma_tgia,) VALUES ('$ma_bviet','$tieude','$ten_bhat','$ma_tloai','$tomtat','$ma_tgia') ")){
             echo "<script>alert('them thanh cong  !');</script>";
         }
         else{
@@ -60,22 +64,47 @@
     $conn->close();
     ?>
     <div class="container">
-    <h1>Thêm tác giả</h1>
+    <h1>Thêm bài viết</h1>
         <form method="POST" action="">
             <div class="form-group">
-                <label for="ten_tgia"> Tên Tác Giả </label>
-                <input name = "ten_tgia" class="form-control" placeholder="VD:nguyen van a">
+                <label for="ma_bviet"> Mã bài viết </label>
+                <input name = "ma_bviet" class="form-control" >
             </div>
 
             <div class="form-group">
-                <label for="ma_tgia">Mã tác giả</label>
-                <input name = "ma_tgia" class="form-control" placeholder="VD: 123 " >
+                <label for="tieude">Tiêu đề</label>
+                <input name = "tieude" class="form-control"  >
             </div>
-            <button type="submit" href="lietketacgia.php" class="btn btn-success" name = "add">Thêm</button>
+            <div class="form-group">
+                <label for="ten_bhat">Tên bài hát</label>
+                <input name = "ten_bhat" class="form-control"  >
+            </div>
+            <div class="form-group">
+                <label for="ma_tloai">Mã thể loại</label>
+                <input name = "ma_tloai" class="form-control"  >
+            </div>
+            <div class="form-group">
+                <label for="tomtat">Tóm tắt</label>
+                <input name = "tomtat" class="form-control" >
+            </div>
+            <div class="form-group">
+                <label for="ma_tgia">Mã tác giả</label>
+                <input name = "ma_tgia" class="form-control"  >
+            </div>
+            <!-- <var><div class="form-group">
+                <label for="ngayviet">Ngày Viết</label>
+                <input name ="ngayviet" type = "datetime" class="form-control"  >
+            </div></var> -->
+            <button type="submit" class="btn btn-success" name = "add">Thêm</button>
         </form>
 </div>
 
-<footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
+
+
+
+
+
+    <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
         <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
