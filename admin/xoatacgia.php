ed91ea36f364caeccb7445237a91b37674801cbd
viet3110 <104310094+viet3110@users.noreply.github.com>
2023-02-28 00:42:04 +0700

    Delete admin directory

--- admin/xoatacgia.php
+++
@@ -1,9 +0,0 @@
-<?php
-//lay du lieu id can xoa
-$id = $_GET['ma_tgia'];
-require_once 'connect.php';
-//cau lenh xoa sql
-$xoa_sql = " DELETE FROM tacgia WHERE ma_tgia=$id "; 
-mysqli_query($conn, $xoa_sql);
-echo "<h1>xóa thành công</h1>";
-?>
