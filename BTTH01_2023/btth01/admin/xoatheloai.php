64e8808563c338659488252d1bcd0ea1bb88c97f
viet3110 <104310094+viet3110@users.noreply.github.com>
2023-02-28 03:35:26 +0700

    Update xoatheloai.php

--- BTTH01_2023/btth01/admin/xoatheloai.php
+++ BTTH01_2023/btth01/admin/xoatheloai.php
@@ -3,7 +3,7 @@
 $id = $_GET['sid'];
 require_once 'connect.php';
 //cau lenh xoa sql
-$xoa_sql = " DELETE FROM tacgia WHERE ma_tloai=$id "; 
+$xoa_sql = " DELETE FROM theloai WHERE ma_tloai=$id "; 
 mysqli_query($conn, $xoa_sql);
 echo "<h1>xóa thành công</h1>";
 header("Location:lietketheloai.php");
